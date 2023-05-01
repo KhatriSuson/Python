a4e2636ad5ea731650a730c72d031183ca8f459a
KhatriSuson <susonkhatri110@gmail.com>
2023-05-01 21:38:28 +0545

    calculator progam in python

---
+++ snake.py
@@ -0,0 +1,10 @@
+import pygame
+import time
+import random
+pygame.init()
+display_width = 800
+display_height = 600
+
+game_display = pygame.display.set_mode((display_width, display.height))
+
+pygame.display.set_caption('Snake Game ')
